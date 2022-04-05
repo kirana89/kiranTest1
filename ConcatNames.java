419fe17a57a21798d3e739c84798fcf8c01c26a5
kirana89 <102513867+kirana89@users.noreply.github.com>
2022-04-05 17:47:02 +0530

    Update ConcatNames.java

--- ConcatNames.java
+++ ConcatNames.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ConcatNames {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter your FirstName");
 		String FirstName=sc.next();
